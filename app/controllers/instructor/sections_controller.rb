204506fd95d92912ce44d2b0258eafd7d3019f21
Nick Parsley <nicholasparsley@gmail.com>
2019-11-28 01:32:15 +0000

    Sections

--- app/controllers/instructor/sections_controller.rb
+++ app/controllers/instructor/sections_controller.rb
@@ -1,6 +1,6 @@
 class Instructor::SectionsController < ApplicationController
   before_action :authenticate_user! 
-  before_action :require_authorized_for_current_course, only: [:create]
+  before_action :require_authorized_for_current_course, only: [:new, :create]
   before_action :require_authorized_for_current_section, only: [:update]
 
   def new
@@ -27,6 +27,12 @@ class Instructor::SectionsController < ApplicationController
   def require_authorized_for_current_course
     if current_course.user != current_user
       render plain: "Unauthorized", status: :unauthorized
+    end
+  end
+
+  def require_authorized_for_current_section
+    if current_section.course.user != current_user
+      return render plain: 'Unauthorized', status: :unauthorized
     end
   end
 
